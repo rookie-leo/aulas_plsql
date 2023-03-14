2f6b5bcb821e6034da8b935abc2b98cff9e8f1dd
rookie-leo <proleonardopa@gmail.com>
2023-03-13 21:16:08 -0300

    Atividade 5

--- aula1.sql
+++ aula1.sql
@@ -1,11 +1,15 @@
 set serveroutput on size 2000;
 DECLARE
-    valor_novo number(6,2);
-    valor_atual number(6,2);
-    porcentagem number(6,2) := 1.08;
-    calculo number(6,2);
+    NUM_FORNEC	NUMBER(5) := &NUM_FORNEC;
+    NOME	VARCHAR2(15) := '&NOME';
+    ENDERECO	VARCHAR2(30) := '&ENDERECO';
+    CPF	VARCHAR2(14) := '&CPF';
+    ESTRELA	NUMBER(1) := &ESTRELA;
+    VENDA	NUMBER(11,2) := &VENDA;
+    COMISSAO	NUMBER(11,2) := &COMISSAO;
 BEGIN
-    valor_atual := 1302;
-    valor_novo := valor_atual * porcentagem;
-    dbms_output.put_line(valor_novo);
+    insert into fornecedor (n_fornec, nome, endereco, cpf, estrela, dt_atualiza, venda, comissao)
+    values (NUM_FORNEC, NOME, ENDERECO, CPF, ESTRELA, SYSDATE, VENDA, COMISSAO);
+    dbms_output.put_line('Registro inserido com sucesso');
 END;
+
