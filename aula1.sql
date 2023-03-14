cd3406bf6ea75aff358f20bd9085c8309f0104a0
rookie-leo <proleonardopa@gmail.com>
2023-03-13 21:15:08 -0300

    Atividade 5

--- aula1.sql
+++ aula1.sql
@@ -1,9 +1,11 @@
+set serveroutput on size 2000;
 DECLARE
     valor_novo number(6,2);
     valor_atual number(6,2);
-    porcentagem number(6,2) := 8/100;
+    porcentagem number(6,2) := 1.08;
     calculo number(6,2);
 BEGIN
+    valor_atual := 1302;
     valor_novo := valor_atual * porcentagem;
-    DBMS_OUTPUT.PUT_LINE(valor_novo);
+    dbms_output.put_line(valor_novo);
 END;
